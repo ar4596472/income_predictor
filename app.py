c1f5208fbc8be55646301084fcc5e86e84105071
Aman Rathor <116510541+ar4596472@users.noreply.github.com>
2024-05-09 21:55:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 
 # Load the dataset (cached using st.cache)
